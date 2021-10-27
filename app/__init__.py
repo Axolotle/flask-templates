f2b2be3de35045baa96f9672357c660e5f3f61be
axolotle <nchesnais@mailoo.org>
2021-10-27 19:48:43 +0200

    add url rule for prod

--- app/__init__.py
+++ app/__init__.py
@@ -38,8 +38,12 @@ def register_blueprints(app):
     app.register_blueprint(client.routes.bp)
     app.register_blueprint(admin.routes.bp, url_prefix=app.config['ADMIN_ROUTE'])
 
-    # allow werkzeug to serve static files from instance_path in dev mode
     if (app.config["ENV"] == 'development'):
+        # allow werkzeug to serve static files from instance_path in dev mode
         with app.app_context():
             from app import uploads
             app.register_blueprint(uploads.bp, url_prefix='/uploads')
+    else:
+        # allow using `url_for('uploads.static', filename="file.jpg")` without registering any blueprints
+        app.add_url_rule("/uploads/<filename>",
+                         endpoint="uploads.static", build_only=True)
